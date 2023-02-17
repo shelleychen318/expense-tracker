ddcc0fe7aa3ec9b87490fc4c51c68d77621752bf
Shelley Chen <shelleychen318@gmail.com>
2023-02-17 11:57:34 -0500

    Initial Commit

---
+++ ExpenseTracker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ExpenseTracker
+//
+//  Created by Shelley Chen on 2023-02-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Shelley Chen on 2023-02-17.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
