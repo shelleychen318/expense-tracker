d806b7c7cccd93a2345fe66156e6a063f178a04d
Shelley Chen <shelleychen318@gmail.com>
2023-02-17 15:39:20 -0500

    formatted date

--- ExpenseTracker/TransactionRow.swift
+++ ExpenseTracker/TransactionRow.swift
@@ -26,7 +26,7 @@ struct TransactionRow: View {
                     .lineLimit(1)
                 
                 // MARK: Transaction Date
-                Text(Date(), format: .dateTime.year().month().day())
+                Text(transaction.dateParsed, format: .dateTime.year().month().day())
                     .font(.footnote)
                     .foregroundColor(.secondary)
                 
