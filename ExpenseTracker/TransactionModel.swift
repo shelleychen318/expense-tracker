34162d59818a6a201e5b27b4f2d01896570c84e1
Shelley Chen <shelleychen318@gmail.com>
2023-02-17 16:00:42 -0500

    displayed transaction amount

--- ExpenseTracker/TransactionModel.swift
+++ ExpenseTracker/TransactionModel.swift
@@ -25,6 +25,10 @@ struct Transaction: Identifiable {
     var dateParsed: Date {
         date.dateParsed()
     }
+    
+    var signedAmount: Double {
+        return type == TransactionType.credit.rawValue ? amount : -amount
+    }
 }
 
 enum TransactionType: String {
--- ExpenseTracker/TransactionRow.swift
+++ ExpenseTracker/TransactionRow.swift
@@ -12,6 +12,12 @@ struct TransactionRow: View {
     
     var body: some View {
         HStack(spacing: 20) {
+            // MARK: Transaction Category Icon
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .fill(Color.icon.opacity(0.3))
+                .frame(width: 44, height: 44)
+            
+            
             VStack(alignment: .leading, spacing: 6) {
                 // MARK: Transaction Merchant
                 Text(transaction.merchant)
@@ -29,9 +35,14 @@ struct TransactionRow: View {
                 Text(transaction.dateParsed, format: .dateTime.year().month().day())
                     .font(.footnote)
                     .foregroundColor(.secondary)
-                
-                
             }
+            
+            Spacer()
+            
+            // MARK: Transaction Amount
+            Text(transaction.signedAmount, format: .currency(code: "USD"))
+                .bold()
+                .foregroundColor(transaction.type == TransactionType.credit.rawValue ? Color.text : .primary)
         }
         .padding([.top, .bottom], 8)
     }
