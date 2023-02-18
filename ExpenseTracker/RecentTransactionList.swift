b40626a63b901823544c75741196e5daf5cd9adb
Shelley Chen <shelleychen318@gmail.com>
2023-02-17 20:21:35 -0500

    displayed recent transactions

--- ExpenseTracker/RecentTransactionList.swift
+++ ExpenseTracker/RecentTransactionList.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct RecentTransactionList: View {
+    @EnvironmentObject var transactionListVM: TransactionListViewModel
+    
     var body: some View {
         VStack {
             HStack {
@@ -29,6 +31,14 @@ struct RecentTransactionList: View {
                 }
             }
             .padding(.top)
+            
+            // MARK: Recent Transaction List
+            ForEach(Array(transactionListVM.transactions.prefix(5).enumerated()), id: \.element) { index, transaction in
+                TransactionRow(transaction: transaction)
+                
+                Divider()
+                    .opacity(index == 4 ? 0 : 1)
+            }
         }
         .padding()
         .background(Color.background)
@@ -38,7 +48,18 @@ struct RecentTransactionList: View {
 }
 
 struct RecentTransactionList_Previews: PreviewProvider {
+    static let transactionListVM: TransactionListViewModel = {
+        let transactionListVM = TransactionListViewModel()
+        transactionListVM.transactions = transactionListPreviewData
+        return transactionListVM
+    }()
+    
     static var previews: some View {
-        RecentTransactionList()
+        Group {
+            RecentTransactionList()
+            RecentTransactionList()
+                .preferredColorScheme(.dark)
+        }
+        .environmentObject(transactionListVM)
     }
 }
