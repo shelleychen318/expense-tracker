39d859276720beea328caa3f65b611d6ee1045ad
Shelley Chen <shelleychen318@gmail.com>
2023-02-17 19:13:27 -0500

    created RecentTransactionList component

---
+++ ExpenseTracker/RecentTransactionList.swift
@@ -0,0 +1,44 @@
+//
+//  RecentTransactionList.swift
+//  ExpenseTracker
+//
+//  Created by Shelley Chen on 2023-02-17.
+//
+
+import SwiftUI
+
+struct RecentTransactionList: View {
+    var body: some View {
+        VStack {
+            HStack {
+                // MARK: Header Title
+                Text("Recent Transactions")
+                    .bold()
+                
+                Spacer()
+                
+                // MARK: Header Link
+                NavigationLink {
+                    
+                } label: {
+                    HStack(spacing: 4) {
+                        Text("See All")
+                        Image(systemName: "chevron.right")
+                    }
+                    .foregroundColor(Color.text)
+                }
+            }
+            .padding(.top)
+        }
+        .padding()
+        .background(Color.background)
+        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+        .shadow(color: Color.primary.opacity(0.2), radius: 10, x: 0, y: 5)
+    }
+}
+
+struct RecentTransactionList_Previews: PreviewProvider {
+    static var previews: some View {
+        RecentTransactionList()
+    }
+}
